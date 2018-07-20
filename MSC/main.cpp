e07b0c6db665cfda32a1f5c5a17f9f543b4a3f17
mac <mac@macs-macbook-pro.local>
2018-07-20 03:12:42 +0200

    Initial Commit

---
+++ MSC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MSC
+//
+//  Created by mac on 20/7/2018.
+//  Copyright © 2018 DvixExtract. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
